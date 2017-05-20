d37b7c5ad18823da00875f64a6c81e40ee408666
root <root@localhost.localdomain>
2017-05-20 18:32:53 +0100

    second

--- models/users.js
+++ models/users.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'); 
-var db = mongoose.connect('mongodb://127.0.0.1/db');
+var db = mongoose.connect('mongodb://chawki008:84835484@ds064299.mlab.com:64299/chat_01');
 var user = new mongoose.Schema({name:String , password:String});
 var users = db.model('users',user);
 module.exports = users;
